de3a6d4665e99d32d6240cd5f91682b3c699475c
Mohamed Ibrahim <mo3dev@gmail.com>
2013-02-17 21:06:55 -0500

    added a HauntdCursor class that will automatically move the cursor accross the screen from the center to the right

---
+++ Mischief/Plots/HauntedCursor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Mischief.Plots
+{
+    class HauntedCursor : IPlot
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SetCursorPos(int x, int y);
+
+        public void Plot()
+        {
+            // move the mouse cursor (pointer) across the current screen from the center
+            var screen = System.Windows.Forms.Screen.FromPoint(Cursor.Position).WorkingArea;
+            Point center = new Point(screen.Left + screen.Width / 2, screen.Top + screen.Height / 2);
+
+            for (int i = 0; i < 300; i++)
+            {
+                SetCursorPos(center.X + i, center.Y);
+                System.Threading.Thread.Sleep(10);
+            }
+        }
+    }
+}
--- Mischief/Plots/MouseBtnSwitcher.cs
+++ Mischief/Plots/MouseBtnSwitcher.cs
@@ -9,12 +9,12 @@ namespace Mischief.Plots
     class MouseBtnSwitcher : IPlot
     {
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        private static extern Int32 SwapMouseButtons(UInt32 buttonSwap);
+        private static extern Int32 SwapMouseButton(Int32 buttonSwap);
 
         public void Plot()
         {
             // swap mouse buttons (0 = normal, 1 = swap)
-            SwapMouseButtons(1);
+            SwapMouseButton(1);
         }
     }
 }
