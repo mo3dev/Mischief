f75e38cb0853ab58252b087891ce5e4ca191d70e
Mohamed Ibrahim <mo3dev@gmail.com>
2013-02-19 00:31:33 -0500

    recreated the project as a standalone console application, next: add a service timer and keep it running depending on command line args (-service = service pattern)

--- Mischief/Program.cs
+++ Mischief/Program.cs
@@ -1,35 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.ServiceProcess;
 using System.Text;
 
 namespace Mischief
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        static void Main()
+        static void Main(string[] args)
         {
-
-#if DEBUG
-            // Debugger is attached, start the service manually and keep it running
-            Service svc = new Service();
-            svc.OnDebug();
-            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
-
-#else
-            // Debugger is not attached
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[] 
-			{ 
-				new Service() 
-			};
-            ServiceBase.Run(ServicesToRun);
-#endif
-
+            RandomPlot();
         }
+
+
+        /// <summary>
+        /// Using the current assembly, get a list of classes that implement the T type (ie. IPlot interface)
+        /// </summary>
+        /// <typeparam name="T">The type (ie. interface) to search for in the program</typeparam>
+        /// <returns></returns>
+        public static IEnumerable<Type> GetPlots<T>()
+        {
+            return System.Reflection.Assembly.GetExecutingAssembly().GetTypes()
+                   .Where(type => typeof(T).IsAssignableFrom(type))
+                   .Where(type => type.FullName != typeof(T).FullName);
+        }
+
+
+        /// <summary>
+        /// Launches 1 random mischief attack from the list of Plot classes in the program
+        /// </summary>
+        static void RandomPlot()
+        {
+            var plots = GetPlots<IPlot>();
+            var plot = plots.ElementAt(new Random().Next(plots.Count()));
+
+            // run the random plot
+            Object o = Activator.CreateInstance(plot);
+            IPlot mischief = (IPlot)o;
+            mischief.Plot();
+        }
+
+
+        /// <summary>
+        /// Unleash a full/largescale attack. Launch all mischief plots (declare war on enemy) one after another
+        /// </summary>
+        static void Nuke()
+        {
+            var plots = GetPlots<IPlot>();
+            foreach (Type plot in plots)
+            {
+                // run the current plot
+                Object o = Activator.CreateInstance(plot);
+                IPlot mischief = (IPlot)o;
+                mischief.Plot();
+            }
+        }
+
     }
 }
--- Mischief/Properties/AssemblyInfo.cs
+++ Mischief/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8f5b3e00-e526-438f-aac0-596ea530f640")]
+[assembly: Guid("75faed18-bf01-471d-944c-4454e2e055e5")]
 
 // Version information for an assembly consists of the following four values:
 //
