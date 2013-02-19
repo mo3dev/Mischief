d6e107b64f17dee1018b5cfa69891bbb187148ee
Mohamed Ibrahim <mo3dev@gmail.com>
2013-02-19 17:04:01 -0500

    incomplete screen rotate (flip)

---
+++ Mischief/Plots/RotateScreen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mischief.Plots
+{
+    class RotateScreen
+    {
+        public void Plot()
+        {
+            SystemSettings.ScreenOrientation = ScreenOrientation.Angle90;
+        }
+    }
+}
